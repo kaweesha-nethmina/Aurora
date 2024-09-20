e7a302d6a9813ab146e47edf35c06b764470dd10
Kaweesha Nethmina <kaweeshaneth420@gmail.com>
2024-09-20 13:17:19 +0530

    handle img start

--- rms-client/src/modules/restuarent/restuarent/components/Menu/MenuItem.tsx
+++ rms-client/src/modules/restuarent/restuarent/components/Menu/MenuItem.tsx
@@ -1,5 +1,3 @@
-// src/components/Menu/MenuItem.tsx
-
 import React from 'react';
 import { MenuItem as MenuItemType } from '../../services/menuService';
 import './menuItem.css';
@@ -9,9 +7,12 @@ interface MenuItemProps {
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
+  // Adjust the image URL to point to the backend server where images are stored
+  const imageUrl = `http://localhost:5000/${item.imageUrl}`; // Update based on your backend URL
+
   return (
     <div className="menu-item">
-      <img src={item.imageUrl} alt={item.name} className="menu-item-image" />
+      <img src={imageUrl} alt={item.name} className="menu-item-image" />
       <h3 className="menu-item-name">{item.name}</h3>
       <p className="menu-item-description">{item.description}</p>
       <p className="menu-item-price">${item.price.toFixed(2)}</p>
